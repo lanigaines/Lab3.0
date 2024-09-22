5ba577cccd6ca92633edf115b9c2ca9256ef451d
lanigaines <lanigaines@live.com>
2024-09-22 04:17:37 -0700

    adding files
    upload after clone

---
+++ main-2.py
@@ -0,0 +1,20 @@
+def main():
+    number = int(input('Enter your input: '))
+
+    """
+    Make your code here
+    """
+
+    if result:
+        print(f'The value {number} is an odd number')
+    else:
+        print(f'The value {number} is an even number')
+
+    ########################################
+    # Do not delete the return statement
+    ########################################
+    return result
+
+
+if __name__ == '__main__':
+    main()
